e0f846920443b4ee5e0e8a26bba73ab4c325e96f
fabiolamariapina <fabiolamariapina@gmail.com>
2020-10-12 14:44:53 -0400

    added id to button element
    in index.html

--- guess-that-lyric/app.js
+++ guess-that-lyric/app.js
@@ -1,7 +1,6 @@
 // when user clicks the begin game button,
 //the first question/lyric/song will appear
-// create event listener for let's
-//play button
 $(() => {
-  //jQuery code goes here
+  // create event listener for let's
+  //play button
 });
