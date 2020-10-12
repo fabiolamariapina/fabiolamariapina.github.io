3389e2c4d5dea6969789b3a829b766b6ccc1420e
fabiolamariapina <fabiolamariapina@gmail.com>
2020-10-12 15:23:15 -0400

    update app.js
    was able to make it so that when the let's play button is clicked, the first lyric/song/question appears

--- guess-that-lyric/app.js
+++ guess-that-lyric/app.js
@@ -1,10 +1,18 @@
-// when user clicks the begin game button,
-//the first question/lyric/song will appear
 $(() => {
   // create event handler for let's
   //play button
+  // source: General Assembly SEI
+  //W03D01 notes
   const $btn = $("#btn");
   $btn.on("click", () => {
-    console.log("button was clicked");
+    // when let's play button is clicked,
+    //the load page will disappear
+    //and the first question will appear
+    // I want to replace description and btn
+    //with question and multiple choice answers
+    // source: https://api.jquery.com/replacewith/
+    const $question = $(".description").replaceWith(
+      "<h3>Yeah, I'm gonna take my horse to the old town road I'm gonna ride 'til I can't no more</h3>"
+    );
   });
 });
