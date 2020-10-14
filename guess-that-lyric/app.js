28d82f7f3abfdc2213a8d351e69b5653ac754102
fabiolamariapina <fabiolamariapina@gmail.com>
2020-10-14 19:53:58 -0400

    Update app.js
    Make it so that when let's play utton is clicked, a random question and answer choice pair is generated

--- guess-that-lyric/app.js
+++ guess-that-lyric/app.js
@@ -41,9 +41,60 @@ const questionsAndAnswers = [
     choice4: "vodka",
   },
 ];
-// console.log(questionsAndAnswers[1].question);
 
 $(() => {
+  // create new array from old array so that it will be
+  //easier to eliminate anything from array that has already been
+  //asked as a questions
+  //source: TA help
+  let newQuestions = questionsAndAnswers;
+  // create new question function
+  // when let's play button is clicked, it will generate a 
+  //random question and answer pair from array of object
+  //source: TA Help
+  const getNewQuestion = () => {
+    // generate random questions and answers when button is clicked
+    const randomIndex = Math.floor(Math.random() * newQuestions.length);
+    const $question = $(".description")
+      .removeClass("description")
+      .addClass("question")
+      .text(newQuestions[randomIndex].question);
+    //create new div between question and button
+    //for answer choices
+    const $choices = $("<div>").addClass("choices");
+    $(".container").append($choices);
+    //answerChoice1
+    const $choice1 = $("<button>")
+      .addClass("choice-1 btn-sm")
+      .text(newQuestions[randomIndex].choice1);
+    $(".choices").append($choice1);
+    //answerChoice2
+    const $choice2 = $("<button>")
+      .addClass("choice-2 btn-sm")
+      .text(newQuestions[randomIndex].choice2);
+    $(".choices").append($choice2);
+    //answerChoice3
+    const $choice3 = $("<button>")
+      .addClass("choice-3 btn-sm")
+      .text(newQuestions[randomIndex].choice3);
+    $(".choices").append($choice3);
+    //answerChoice4
+    const $choice4 = $("<button>")
+      .addClass("choice-4 btn-sm ")
+      .text(newQuestions[randomIndex].choice4);
+    $(".choices").append($choice4);
+    //remove item from array once it has been used as a question
+    newQuestions.splice(randomIndex, 1);
+    //change let's play button to Answer button
+    // source:
+    //https://www.tutorialrepublic.com/faq/how-to-change-the-text
+    //-of-a-button-using-jquery.php
+    // change class of answer button but keep same styling of let's play
+    //button
+    // source: https://stackoverflow.com/questions/3959236/
+    //how-to-change-class-name-of-an-element-by-jquery
+    const $answerButton = $("#btn").html("Answer").addClass("answer-button");
+  };
   // create event handler for let's
   //play button
   // source: General Assembly SEI
@@ -59,43 +110,8 @@ $(() => {
     // add text of object in questionAndAnswers array
     // make it so that when let's play button clicks,
     //a random array element (aka a random song/missing lyrics) appears
-    const $question = $(".description")
-      .removeClass("description")
-      .addClass("question")
-      .text(questionsAndAnswers[0].question);
-    //create new div between question and button
-    //for answer choices
-    const $choices = $("<div>").addClass("choices");
-    $(".container").append($choices);
-    //answerChoice1
-    const $choice1 = $("<button>")
-      .addClass("choice-1 btn-sm")
-      .text(questionsAndAnswers[0].choice1);
-    $(".choices").append($choice1);
-    //answerChoice2
-    const $choice2 = $("<button>")
-      .addClass("choice-2 btn-sm")
-      .text(questionsAndAnswers[0].choice2);
-    $(".choices").append($choice2);
-    //answerChoice3
-    const $choice3 = $("<button>")
-      .addClass("choice-3 btn-sm")
-      .text(questionsAndAnswers[0].choice3);
-    $(".choices").append($choice3);
-    //answerChoice4
-    const $choice4 = $("<button>")
-      .addClass("choice-4 btn-sm ")
-      .text(questionsAndAnswers[0].choice4);
-    $(".choices").append($choice4);
-    //change let's play button to Answer button
-    // source:
-    //https://www.tutorialrepublic.com/faq/how-to-change-the-text
-    //-of-a-button-using-jquery.php
-    // change class of answer button but keep same styling of let's play
-    //button
-    // source: https://stackoverflow.com/questions/3959236/
-    //how-to-change-class-name-of-an-element-by-jquery
-    const $answerButton = $("#btn").html("Answer").addClass("answer-button");
+
+    getNewQuestion();
   });
 });
 
